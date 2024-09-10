a64088fe99ecb1266b568dca9441a0c31dc3c8d1
veerendra19codes <veerendragumate@gmail.com>
2024-09-10 15:30:06 +0530

    init

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className="min-h-screen bg-black flex justify-center items-center">
+            <div className="w-[80%] h-[80%] bg-white">
+                Form
+            </div>
+        </div>
+    )
+}
