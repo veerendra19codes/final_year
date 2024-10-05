d327f76fa2b24f48c367af7e0f3d4fefaac7ea7d
YashKaliaS <kaliamonica122@gmail.com>
2024-10-05 15:42:50 +0530

    new post society route

---
+++ src/app/api/society/route.js
@@ -0,0 +1,27 @@
+import { connectdb } from "@/lib/db";
+import {Society} from "@lib/models" 
+import { NextRequest, NextResponse } from "next/server";
+export async function POST(req){
+    try {
+        //connect wiht database each time
+        await connectdb();
+        //destructure your things
+        const{name,address,image,secretary}=await req.json();
+        console.log(name,address,image,secretary);
+        const newSociety=await Society.create({
+            name,
+            address,
+            image,
+            secretary
+        })
+        return NextResponse.json({
+            message:"added society details successfully", 
+        })
+        
+    } catch (error) {
+        console.error(error.message);
+        return NextResponse.status(500).json({
+            message:"Error creating Society"
+        })
+    }
+}
